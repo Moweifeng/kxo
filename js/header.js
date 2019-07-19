195281e541fab38321be9ab1538144e78393ef39
moweifeng <957796031@qq.com>
2019-07-19 19:29:18 +0800

    7/19

---
+++ js/header.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    var header_list_a = document.querySelectorAll(".header-list-a");
+    var header_list = document.querySelectorAll(".header-list");
+    // for (let i = 0; i < header_list_a.length; i++) {
+    //     var element = header_list_a[i];
+    //     var content=  header_list[i];   
+    //     }
+    //     header_list_a[1].onmouseover=function(){
+    //         header_list[1].style.display='block';
+    // }
+    for (let i = 0; i < header_list_a.length; i++) {
+        header_list_a[i].onmouseenter = function () {
+            // for (let item of header_list) {
+            //     item.style.display = "none";
+            // }
+            for (let j = 0; j < header_list.length; j++) {
+                header_list[j].style.display = 'none';
+            }
+            header_list[i].style.display = "block";
+        }
+        header_list[i].onmouseenter = function () {
+            this.style.display = "block";
+        }
+        header_list[i].onmouseleave = function () {
+            this.style.display = "none";
+        }
+    }
+}
+
+
+
